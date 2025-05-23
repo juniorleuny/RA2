728a9fe2587444975db2e9fc3202ed82c8ee1c37
juniorleuny <Leunyjunior034@gmail.com>
2025-05-23 13:56:11 -0300

    Add files via upload
    Suma de dos numeros

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el primer número entero:");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el segundo número entero:");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            int suma = numero1 + numero2;
+            int resta = numero1 - numero2;
+            int multiplicacion = numero1 * numero2;
+            double division;
+
+            
+            if (numero2 != 0)
+            {
+                division = (double)numero1 / numero2;
+            }
+            else
+            {
+                division = double.NaN; 
+            }
+
+            
+            Console.WriteLine($"Suma: {suma}");
+            Console.WriteLine($"Resta: {resta}");
+            Console.WriteLine($"Multiplicación: {multiplicacion}");
+            if (numero2 != 0)
+            {
+                Console.WriteLine($"División: {division}");
+            }
+            else
+            {
+                Console.WriteLine("División: No se puede dividir por cero.");
+            }
+        }
+    }
+}
